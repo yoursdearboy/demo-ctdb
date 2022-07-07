e0df2de647c9a7d8aa147456ee628977cf8d75e6
Kirill Voronin <yoursdearboy@gmail.com>
2022-07-07 22:46:24 +0000

    Added Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    res = db.engine.execute("SELECT 1").scalar()
+    return f"<p>Hello, {res}!</p>"
--- db.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from sqlalchemy import create_engine
-
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-res = engine.execute("SELECT 1").scalar()
-print(res)
